5641bb03ff08e1bf52fc15cd625ca346fa0a8430
Leyang Wang <111491853+Leyangw@users.noreply.github.com>
2023-10-27 01:50:05 +0100

    Delete noise.py

--- noise.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def get_noisy_image(img_np, sigma):
-    Gauss_noise = np.random.normal(scale=sigma, size=img_np.shape)
-    img_noisy_np = np.clip(img_np + Gauss_noise, 0, 1).astype(np.float32)
-
-    return img_noisy_np
