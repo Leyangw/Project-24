cde30a4baaa7081bfe151105bc6da6554dc8ca78
Leyang Wang <111491853+Leyangw@users.noreply.github.com>
2024-02-16 04:57:11 +0000

    Add files via upload

--- Deep Image Prior/denosing.py
+++ Deep Image Prior/denosing.py
@@ -2,30 +2,35 @@ import torch
 from torch.optim import Adam
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
-
+from DEQ_solver.solver import *
+from torch.autograd import gradcheck
 import numpy as np
 from PIL import Image
 from noise import *
 from Unet import *
 import matplotlib.pyplot as plt
-
+import random
+from E_SUREloss import *
 from dival.measure import PSNRMeasure
-
+PSNRm = PSNRMeasure(short_name='a',data_range=1)
+import json
+import os
+seed_everything(1)
 #hyperparameter
-num_iter = 4000
-lr=0.1
-show_evey =50
+num_iter = 5500
+lr=0.01
+show_evey =100
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 loss_lst = []
 PSNR_lst = []
+PSNR1_lst=[]
 ToTensor = ToTensor()
 
 #network setup
-net1 = UnetModel(3,3,8,5,0).to(device)
-optimizer = Adam(net1.parameters(), lr=lr,weight_decay=0)
+net1 = UnetModel(3,3,4,5,0).to(device)
 mse = torch.nn.MSELoss().to(device)
-#s  = sum([np.prod(list(p.size())) for p in net.parameters()]);
-#print ('Number of params: %d' % s)
+s = sum([np.prod(list(p.size())) for p in net1.parameters()]);
+print ('Number of params: %d' % s)
 
 #data post-processing
 pic = r'D:\pythonProject\dataset\train\F16_GT.png'
@@ -33,49 +38,50 @@ image = Image.open(pic)
 data = ToTensor(image.copy())
 data = torch.unsqueeze(data,0)
 noisy_data = add_gaussian_noise(data,std_dev=25).to(device,dtype=torch.float)
-z = add_gaussian_noise(torch.zeros(1,3,512,512),std_dev=1).to(device,dtype=torch.float)
-z = DataLoader(z,batch_size=1)
 
-def main(num_iter=3000,rep_psnr=True):
+optimizer = Adam(net1.parameters(), lr=lr,weight_decay=0)
+
+z0 = add_gaussian_noise(torch.zeros(1,3,512,512).to(device),std_dev=1).requires_grad_()
+#zl= torch.zeros(1,3,512,512).to(device)
+
+def main(num_iter=3000,rep_psnr=True,show_evey=5,exp_weight=0.99,out_avg = None,z1=None,noise1 = None):
     for i in range(num_iter):
-        for k in z:
-    #        for name, param in net1.named_parameters():
-     #           if param.grad is not None:
-     #               print(f"Layer: {name}, Gradient Norm: {param.grad.norm().item()}")
-            out = net1(k)
+        input = add_gaussian_noise(noisy_data,std_dev=1).to(device,dtype=torch.float).requires_grad_()
+        out = net1(input)
 
-            if rep_psnr and i%show_evey == 0:
-                print(f'----------{i}--------')
-                with torch.no_grad():
-                    psnr = PSNRMeasure(short_name=f'psnr_{i}').apply(out.detach().cpu(),data)
+        if out_avg is None:
+            out_avg = out
+        else:
+            out_avg = out_avg * exp_weight + out.detach() * (1 - exp_weight)
+
+        if rep_psnr:
+            with torch.no_grad():
+                psnr = PSNRm(out.detach().cpu(),data)
+                psnr1=PSNRm(out_avg.detach().cpu(),data)
+                if i%show_evey == 0:
+                    print(f'----------{i}--------')
                     print(psnr)
-                    PSNR_lst.append(psnr)
+                    print(psnr1)
+                PSNR_lst.append(psnr)
+                PSNR1_lst.append(psnr1)
+        divergence = divergence_new(input, out)
+        lossfn = torch.sum(mse(out,noisy_data)+ 2 * divergence*25 -25)
+  #      print(divergence.item())
+      #      loss1 = mse(out3, noisya1) + divergence
 
-            lossfn = mse(out,noisy_data)
-          #  print(loss.item())
-        #    loss_lst.append(loss.item())
+        optimizer.zero_grad()
+        lossfn.backward()
+        optimizer.step()
 
-            optimizer.zero_grad()
-            lossfn.backward()
-            optimizer.step()
-
-            if i%show_evey == 0:
-                print(lossfn.item())
-            loss_lst.append(lossfn.item())
+        if i%show_evey == 0:
+            print(lossfn.item())
+        loss_lst.append(lossfn.item())
 
 if __name__ == '__main__':
-    main(num_iter=num_iter)
+    main(num_iter=num_iter,show_evey=show_evey)
+    print(max(PSNR_lst))
+    print(max(PSNR1_lst))
+    print(PSNR1_lst)
     x_values = [i for i in range(num_iter)]
-    plt.plot(x_values, loss_lst, marker='o', linestyle='-', color='b', label='Data Points')
-    plt.plot(PSNR_lst, label='Data 2', color='r', marker='s', linestyle='--')
-    plt.xlabel('X-axis Label')
-    plt.ylabel('Y-axis Label')
-    plt.title('Loss')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-
-
 
 
